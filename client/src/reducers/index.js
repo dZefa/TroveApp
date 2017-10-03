270889684e1cc85022a916a990c22a37a31d7d7e
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-03 14:49:11 -0700

    [fix] reducer outline

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,15 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import NavBar from '../components/NavBar';
-import Home from '../components/Home';
-import Men from '../components/Men';
-import Women from '../components/Women';
-import Dashboard from '../components/Dashboard';
-// import Login from '../components/Login';
-import SearchResult from '../components/SearchResult';
-import UserWardrobe from '../components/UserWardrobe';
-// import Footer from '../components/Footer';
 
 const TroveReducer = combineReducers({
   NavBar,
@@ -17,10 +8,10 @@ const TroveReducer = combineReducers({
   Men,
   Women,
   Dashboard,
-  // Login,
+  Login,
   SearchResult,
   UserWardrobe,
-  // Footer,
+  Footer,
   routing: routerReducer,
 });
 
