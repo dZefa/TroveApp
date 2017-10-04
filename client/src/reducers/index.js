8c1281c1db661f434fecd86afcf665ab2a1ddb5a
Daniel Chong <danchong625@gmail.com>
2017-10-04 14:52:01 -0700

    [Init] Implement Cart into reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,12 @@ import { routerReducer } from 'react-router-redux';
 import Item from './itemsReducer';
 import Dashboard from './dashboardReducers';
 import Search from './searchReducer'
+import Cart from './cartReducers';
 
 const TroveReducer = combineReducers({
   // NavBar,
   // Home,
+  Cart,
   Item,
   Dashboard,
   // Login,
