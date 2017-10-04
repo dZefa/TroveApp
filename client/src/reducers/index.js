4ae19507712e0dda209c282c66dc158f871833d3
Daniel Chong <danchong625@gmail.com>
2017-10-03 17:21:56 -0700

    [Add] Add dashboard reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import Item from './itemsReducer';
+import Dashboard from './dashboardReducers';
 
 const TroveReducer = combineReducers({
   NavBar,
