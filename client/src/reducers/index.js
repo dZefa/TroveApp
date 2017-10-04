b870656688025f4bafe05d2ddc921144023d6442
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-03 19:59:28 -0700

    [redux] add items in reducers index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import Item from './itemsReducer';
 
 const TroveReducer = combineReducers({
   NavBar,
