16f9a6411529d07ef03fd5c9d4e861ed3ab3eced
Daniel Chong <danchong625@gmail.com>
2017-10-03 19:36:41 -0700

    [Refactor] Refactor to implement redux

--- client/src/components/Dashboard/Upload.js
+++ client/src/components/Dashboard/Upload.js
@@ -1,93 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Modal, ModalManager, Effect } from 'react-dynamic-modal';
 import { WithContext as ReactTags } from 'react-tag-input';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as dashboardActions from '../../actions/dashboardActions';
 
 class Upload extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      tags: [],
-      tagsToPost: [],
-      itemname: '',
-      rentee_id: this.props.passUser.id,
-      brand: '',
-      price: 0.0,
-      size: '',
-      sex: '',
-      image: '',
-      status: 'Available'
-    }
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleAddition = this.handleAddition.bind(this);
-    this.handleDrag = this.handleDrag.bind(this);
-    this.handlePost = this.handlePost.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.parseThruTags = this.parseThruTags.bind(this);
-  }
-
-  parseThruTags() {
-    const clone = this.state.tags.slice();
-    for (let i = 0; i < clone.length; i++) {
-      this.state.tagsToPost.push(clone[i].text);
-    }
-    return JSON.stringify(this.state.tagsToPost);
-  }
-
-  handleInput(prop, input) {
-    this.state[prop] = input;
-    // console.log(prop, ':', this.state[prop]);
-  }
-
-  handleDelete(i) {
-    let tags = this.state.tags;
-    tags.splice(i, 1);
-    this.setState({tags: tags});
-  }
-
-  handleAddition(tag) {
-      let tags = this.state.tags;
-      tags.push({
-          id: tags.length + 1,
-          text: tag
-      });
-      this.setState({tags: tags});
-  }
-
-  handleDrag(tag, currPos, newPos) {
-      let tags = this.state.tags;
-
-      // mutate array
-      tags.splice(currPos, 1);
-      tags.splice(newPos, 0, tag);
-
-      // re-render
-      this.setState({ tags: tags });
-  }
-
-  handlePost() {
-    axios.post('/api', {
-      rentee_id: this.state.rentee_id,
-      itemname: this.state.itemname,
-      image: this.state.image,
-      brand: this.state.brand,
-      price: this.state.price,
-      size: this.state.size,
-      sex: this.state.sex,
-      tag: this.parseThruTags(),
-      status: this.state.status
-    })
-    .then(() => {
-      console.log('Upload success');
-    })
-    .catch(err => {
-      console.log('Upload err:', err);
-    })
   }
 
   render(){
-    const { text,onRequestClose } = this.props;
-    const { tags, suggestions } = this.state;    
+    const { user, actions } = this.props;
+
     const modalStyle = {
       overlay: {
         backgroundColor : 'transparent'
@@ -98,52 +24,95 @@ class Upload extends Component {
         width: '500px'
       }
     }
+    
+    // UPDATE USER VALUE AFTER AUTH REDUX
+    let itemToPost = {
+      rentee_id: user.username,
+      itemname: document.getElementById('UploadInputItemName').value,
+      image: document.getElementById('UploadInputIMGurl').value,
+      brand: document.getElementById('UploadInputBrand').value,
+      price: parseFloat(document.getElementById('UploadInputRetailPrice').value).toFixed(2),
+      size: '',
+      sex: '',
+      tag: [],
+      status: 'Available'
+    }
+
+    const handleDrag = (item, currPos, newPos) => {
+      itemToPost.tag.splice(currPos, 1);
+      itemToPost.tag.splice(newPos, 0, item);
+    };
+
+    const handleAddition = (tag) => {
+      itemToPost.tag.push({
+          id: tag.length + 1,
+          text: tag
+      });
+    };
+    
+    const handleDelete = (i) => {
+      itemToPost.tag.splice(i, 1);
+    };
+
     return (
        <Modal onRequestClose={onRequestClose} effect={Effect.SlideFromBottom} style={modalStyle} >
          <div className='upload-section'>
-          <div className='upload-input'><input className='form-control' type="text" placeholder="Brand" ref="input" 
-          onChange = { (e) => {this.handleInput('brand', e.target.value) }} />
+          <div className='upload-input'>
+            <input className='form-control' type="text" placeholder="Brand" ref="input" id="UploadInputBrand" />
           </div>
-          <div className='input-group upload-input'><input className='form-control' type="text" placeholder="Item Name" ref="input" 
-          onChange = { (e) => {this.handleInput('itemname', e.target.value) }}/>
+          <div className='input-group upload-input'>
+            <input className='form-control' type="text" placeholder="Item Name" ref="input" id="UploadInputItemName" />
           </div>
-          <div className='input-group upload-input'><input className='form-control' type="text" placeholder="Retail Price" ref="input" 
-          onChange = { (e) => {this.handleInput('price', parseFloat(e.target.value).toFixed(2)) }}/>
+          <div className='input-group upload-input'>
+            <input className='form-control' type="text" placeholder="Retail Price" ref="input" id="UploadInputRetailPrice" />
           </div>
-          <div className='input-group upload-input'><input className='form-control' type="text" placeholder="Image URL" ref="input" 
-          onChange = { (e) => {this.handleInput('image', e.target.value) }}/>
+          <div className='input-group upload-input'>
+            <input className='form-control' type="text" placeholder="Image URL" ref="input" id="UploadInputIMGurl" />
           </div>
           <div className='upload-input'>
             Size:
             <button className="btn btn-secondary btn-sm upload-btn" type="button"
-            onClick = { () => {this.handleInput('size', 'Small') }}>S</button>
+            onClick = { () => {itemToPost.size = 'Small'}}>S</button>
             <button className="btn btn-secondary btn-sm upload-btn" type="button"
-            onClick = { () => {this.handleInput('size', 'Medium') }}>M</button>
+            onClick = { () => {itemToPost.size = 'Medium'}}>M</button>
             <button className="btn btn-secondary btn-sm upload-btn" type="button"
-            onClick = { () => {this.handleInput('size', 'Large') }}>L</button>
+            onClick = { () => {itemToPost.size = 'Large'}}>L</button>
           </div>
           <div className='upload-input'>
             Sex:
             <button className="btn btn-secondary btn-sm upload-btn" type="button"
-            onClick = { () => {this.handleInput('sex', 'M') }}>M</button>
+            onClick = { () => {itemToPost.sex = 'M'}}>M</button>
             <button className="btn btn-secondary btn-sm upload-btn" type="button"
-            onClick = { () => {this.handleInput('sex', 'F') }}>F</button>
+            onClick = { () => {itemToPost.sex = 'F'}}>F</button>
           </div>
           <div className='upload-input tag-input'>
-            <ReactTags tags={tags}
-                suggestions={suggestions}
-                handleDelete={this.handleDelete}
-                handleAddition={this.handleAddition}
-                handleDrag={this.handleDrag} />
+            <ReactTags tags={itemToPost.tag}
+                handleDelete={handleDelete}
+                handleAddition={handleAddition}
+                handleDrag={handleDrag} />
           </div>
           <button className="btn wardrobe-btn-color"
-          onClick={ () => { this.handlePost(); ModalManager.close(); }}>Upload</button>
+            onClick={() => { 
+              actions.postItem(itemToPost); 
+              ModalManager.close(); 
+            }}
+          >Upload</button>
         </div>
        </Modal>
     );
   }
 }
 
-// Tags
+const uploadState = (store) => {
+  return {
+    user: this.props.user // UPDATE WHEN AUTH REDUX DONE
+  };
+};
 
-export default Upload;
+const uploadDispatch = (dispatch) => {
+  return {
+    actions: bindActionCreators(dashboardActions, dispatch),
+  }
+}
+
+export default connect(uploadState, uploadDispatch)(Upload);
