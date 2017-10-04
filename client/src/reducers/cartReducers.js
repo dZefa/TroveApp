22e8eca5a4cb4b29b39c697976045bbb9256ff06
Daniel Chong <danchong625@gmail.com>
2017-10-03 21:13:55 -0700

    [Add] Add cart actions and reducers for redux

---
+++ client/src/reducers/cartReducers.js
@@ -0,0 +1,34 @@
+initialState = {
+  cart: [],
+  viewCart: false,
+};
+
+const cartReducer = (state=initialState, action) => {
+  switch(action.type) {
+    case 'CART_ADD_FULFILLED': {
+      return Object.assign({}, state, {
+        cart: state.cart.concat(action.payload)
+      })
+    }
+    case 'CART_ITEM_DELETE_FULFILLED': {
+      return Object.assign({}, state, {
+        cart: state.cart.filter(item => {
+          return item.id === action.payload ? null : item;
+        })
+      })
+    }
+    case 'SHOW_CART': {
+      return Object.assign({}, state, {
+        viewCart: !state.viewCart
+      })
+    }
+    case 'EMPTY_CART': {
+      return Object.assign({}, state, {
+        cart: []
+      })
+    }
+    default: {
+      return state;
+    }
+  }
+}
