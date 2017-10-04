4d50aa12283fe33e35a196dda86ef0b888f3903a
Daniel Chong <danchong625@gmail.com>
2017-10-03 17:14:19 -0700

    [Init] Initialize Redux

--- client/src/components/Dashboard/Archive.js
+++ client/src/components/Dashboard/Archive.js
@@ -1,77 +1,28 @@
 // This component will display logged user's history of rented items
 import React, { Component } from 'react';
-import axios from 'axios';
-import Loading from '../../components/Loading';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import Loading from './Loading';
 import ArchiveItem from './ArchiveItem';
+
+import * as dashboardActions from '../actions/dashboardActions';
 
 class Archive extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      userRentedTrx: [],
-      allRentedItems: []
-    }
-    this.fetchTrx = this.fetchTrx.bind(this);
-    this.fetchItems = this.fetchItems.bind(this);
   }
 
-  componentDidMount() {
-    this.fetchTrx(this.fetchItems);
-      // .then(() => {
-      // })
+  componentWillMount() {
+    this.props.actions.fetchTrx(this.props.user.id);
   }
-
-  //Fetch all current user's rented trx
-  fetchTrx(cb) {
-    let currRentedTrx = this.state.userRentedTrx;
-    axios.get(`/api/renttrx/renter/${this.props.sqlUser.id}`)
-      .then(({data}) => {
-        console.log('this is data: ', data)
-        data.forEach(trx => {
-          currRentedTrx.push(trx);
-        })
-      })
-      .then(() => {
-        this.setState({
-          userRentedTrx: currRentedTrx
-        }, cb())
-      })
-
-      .catch(err => console.log('err in adding rented items ', err));
-  }
-
-  //Fetch all items current user has rented
-  fetchItems() {
-    let userTrx = this.state.userRentedTrx;
-    let currRentedItems = this.state.allRentedItems;
-
-    userTrx.forEach(trx => {
-      let itemId = trx.item_id;
-      axios.get(`/api/item/${itemId}`)
-        .then(({data}) => {
-          console.log('THIS IS DATA: ', data)
-          data.forEach(item => {
-            item.startDate = trx.startDate;
-            item.endDate = trx.endDate;
-            console.log('THIS IS ITEM: ', item)
-            currRentedItems.push(item)
-          })
-          this.setState({
-            allRentedItems: currRentedItems
-          })
-        })
-    })
-  }
-
 
   render() {
-    console.log('this is state in archive', this.state)
-    // console.log('in archive page, ', this.props.sqlUser)
     return (
       <div>
         <div className='row'>
-          {this.state.allRentedItems.map(item => 
-            <ArchiveItem passItem={item} key={item.id} /> 
+          {this.props.rentedItems.map(item => 
+            <ArchiveItem rentItem={item} key={item.id} /> 
           ).reverse()}
         </div>
       </div>
@@ -79,4 +30,17 @@ class Archive extends Component {
   }
 }
 
-export default Archive;
+const archiveState = (store) => {
+  return {
+    user: this.props.user, // UPDATE THIS WHEN AUTH REDUX DONE
+    rentedItems: store.dashboard.rentedItems
+  };
+};
+
+const archiveDispatch = (dispatch) => {
+  return {
+    actions: bindActionCreators(dashboardActions, dispatch)
+  };
+};
+
+export default connect(archiveState, archiveDispatch)(Archive);
