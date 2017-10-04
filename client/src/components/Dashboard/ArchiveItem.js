524b7d48ddb79b592196e50df9dea95936b16364
Daniel Chong <danchong625@gmail.com>
2017-10-04 12:19:33 -0700

    [Refactor] Refactor to implement redux and clean up code

--- client/src/components/Dashboard/ArchiveItem.js
+++ client/src/components/Dashboard/ArchiveItem.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import Loading from '../../components/Loading';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
 import moment from 'moment';
 
 class ArchiveItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    console.log('in archiveItem', this.props.passItem)
+    const { rentItem } = this.props;
     return (
       <div className='col-md-3 wow fadeIn animated'>
         
           <div className='list-section-wrap'>
             <div className='list-section-picture'>
-                <img src={this.props.passItem.image} ></img>
+                <img src={rentItem.image} ></img>
             </div>
             <div className='list-info'>
               <div>
-                {this.props.passItem.brand}
+                {rentItem.brand}
               </div>
                <div className='list-item-name'>
-                {this.props.passItem.itemname}
+                {rentItem.itemname}
               </div>
               <br/>
               <div>
-                {moment(this.props.passItem.startDate).format("l")}  -  {moment(this.props.passItem.endDate).format("l")}
+                {moment(rentItem.startDate).format("l")}  -  {moment(rentItem.endDate).format("l")}
               </div>
                <div>
-                <span className='list-price-retail line-through'> ${this.props.passItem.price} </span>
-                <span className='list-price-rental'> ${Math.floor(this.props.passItem.price * 0.07)} </span>
+                <span className='list-price-retail line-through'> ${rentItem.price} </span>
+                <span className='list-price-rental'> ${Math.floor(rentItem.price * 0.07)} </span>
               </div>
             </div>
           </div>
